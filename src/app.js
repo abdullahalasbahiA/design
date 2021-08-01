2300a24b5ee81dcb617380abe1875d9d51923d3a
Abdullah Alasbahi <abdullahalasbahia@gmail.com>
2021-08-01 14:52:51 +0200

    project ready

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import "./scss/style.scss";
+// import style from './css/style.css';
+import component from './component.js';
+
+console.log("app.js File");
+
+document.body.append(component())
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+// We are going to install html-webpac... using npm
+// This is going to be another dependence inside of package.json file
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development', // production
+    entry: {
+        main: path.resolve(__dirname, 'src/app.js'), // label to this perticular file
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        // assetModuleFilename: '[name][ext]',
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer:{
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 5001, //default 8080
+        open: true, // launch the browser
+        hot: true, // hot module reload
+        watchContentBase: true,
+    },
+    // loaders
+    module: {
+        rules: [
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+              ],},
+            // images
+            { test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, type:'asset/resource' },
+            // babel for Javascript ES6 > ES5
+            { test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['@babel/preset-env']
+                  }
+              }
+            }
+        ]
+    },
+    // plugins
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Just a Demo',
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'src/temp.html')
+    })],
+}
