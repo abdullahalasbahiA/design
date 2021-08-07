35fc06d7d1dac644fdabd6c469f3c2dcdc160bdf
Abdullah Alasbahi <abdullahalasbahia@gmail.com>
2021-08-07 10:50:56 +0200

    [section why choose us] good in desktop

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import "./scss/style.scss";
+
 import component from './component.js';
 // import style from './css/style.css';
-
+// import swiper from '../node_modules/swiper/swiper-bundle';
 const portfolio_link = document.getElementById("portfolio");
 const drop_down_menu = document.getElementById("dd-menu");
 
@@ -9,7 +9,8 @@ const drop_down_menu = document.getElementById("dd-menu");
 
 // });
 
-
+import "./scss/style.scss";
+import "C:/Users/abdul/Desktop/HardPoints/designing/design/node_modules/@fortawesome/fontawesome-free/js/all.js";
 
 // google icon
 // document.body.append(component())
